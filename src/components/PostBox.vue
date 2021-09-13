600f67514d6f983192f59ec69e1f0850b4b4d6dd
Kim HyunWoo <stories2@naver.com>
2021-09-13 17:17:48 +0900

    #1 post box card ui

--- src/components/PostBox.vue
+++ src/components/PostBox.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="postbox">
-    <img />
+    <div class="thumbnail-img"></div>
     <div>
-        <h3></h3>
-        <pre></pre>
-        <div></div>
+        <h4>This is title</h4>
+        <p>This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.This is description.</p>
+        <div class="text-date">2021년 9월 4일</div>
         <hr>
-        <div>
-            <img />
-            <span>by <span>stories2</span></span>
-            <font-awesome-icon icon="heart" />
-            <span>105</span>
+        <div class="footer">
+            <img class="profile-img" src="../assets/logo.png"/>
+            <span class="writer">by <span class="bold">stories2</span></span>
+            <font-awesome-icon icon="heart" class="gap_margin_5px_horizontal"/>
+            <span class="gap_margin_5px_horizontal">105</span>
         </div>
     </div>
   </div>
@@ -29,4 +29,65 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#postbox {
+    width: 300px;
+    margin: 5px;
+    border-radius: 10px;
+    background-color: white;
+    padding: 0 5px 5px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    transition: .4s;
+}
+#postbox:hover {
+    transform: translate(0, -30px);
+}
+.thumbnail-img {
+    width: 100%;
+    height: 150px;
+    background-image: url('../assets/logo.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+.profile-img {
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+}
+
+p {
+    height: 150px;
+    overflow:hidden;
+    text-overflow:ellipsis;
+    overflow-wrap: break-word;
+    word-break: break-word;
+    text-align: left;
+}
+
+h4 {
+    text-align: left;
+}
+
+.text-date {
+    text-align: left;
+    font-size: 0.9em;
+}
+
+.footer {
+    display: flex;
+    flex-direction: row;
+}
+
+.writer {
+    flex-grow: 1;
+    text-align: left;
+}
+
+.writer .bold {
+    font-weight: bold;
+}
+
+.gap_margin_5px_horizontal {
+    margin: 0 5px;
+}
 </style>
