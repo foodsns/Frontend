10ecb649592352b958fd20adcf82fbd45a7f444e
jini031104 <90189794+jini031104@users.noreply.github.com>
2021-09-14 20:25:44 +0900

    스크롤바 추가
    스크롤바 추가

---
+++ src/components/Scrollbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class = "Scrollbar">
+        <form>
+            거리조절:
+            <input type="range" min="0" max="100" list="tmeperatures">
+            <datalist id="tmeperatures">
+                <option value="15" label="Low" />
+                <option value="50" label="Medium" />
+                <option value="85" label="High" />
+            </datalist>
+        </form>
+    </div>
+</template>
