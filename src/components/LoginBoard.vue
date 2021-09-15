cc2c472a3d5a47e8fae0a066aa495cae80ce6fce
Kim HyunWoo <stories2@naver.com>
2021-09-15 13:25:12 +0900

    #9 Login board test case added

--- src/components/LoginBoard.vue
+++ src/components/LoginBoard.vue
@@ -5,7 +5,7 @@
             <b-col cols="12" md="8" lg="5" xl="4" xxl="4" align-self="center">
                 <b-card title="로그인 / 회원가입" sub-title="아래 SNS 인증을 통해 로그인 하세요.">
                     <google-sign @sign-state="onAuthStateChanged" @sign-error="onError"></google-sign>
-                    <b-card-text style="color: red">
+                    <b-card-text style="color: red" id="loginError">
                         {{ error }}
                     </b-card-text>
                 </b-card>
---
+++ test/unit/specs/LoginBoard.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from '@vue/test-utils'
+import LoginBoard from '@/components/LoginBoard'
+
+const factory = (propsData) => {
+    return shallowMount(LoginBoard, {
+      propsData: {
+        ...propsData
+      }
+    })
+}
+
+describe('LoginBoard.vue', () => {
+    it('Sign in / up error msg must show', () => {
+        const loginBoard = factory({
+        })
+        loginBoard.vm.onError('This is error')
+        expect(loginBoard.vm.error).toBe('This is error')
+        loginBoard.setData({error: 'This is error'})
+        .then(() => {
+            expect(loginBoard.vm.$el.querySelector('#loginError').textContent).toBe('This is error')
+        })
+    })
+})
