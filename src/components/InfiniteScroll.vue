a86117ce0445310c204f86a551fffa0f77e00415
Kim HyunWoo <stories2@naver.com>
2021-09-27 18:56:30 +0900

    #3 Calculate append list point

--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="infinitescroll" v-bind:style="`height: ${threshold}px`">
-    Hello?
+    <span v-if="appendHeightCalc">Hello?</span>
+    <span v-else>Bye</span>
   </div>
 </template>
 
@@ -42,6 +43,20 @@ export default {
         },
         scrollTop: function (val) {
             console.log(`clientHeight: ${this.clientHeight}, scrollHeight: ${this.scrollHeight}, scrollTop: ${this.scrollTop}, threshold: ${this.threshold}`)
+            const height = (this.clientHeight + this.scrollTop) - (this.scrollHeight - this.threshold)
+            console.log(`ch + st >= sh - th == ${this.clientHeight + this.scrollTop} >= ${this.scrollHeight - this.threshold} : ${height}`)
+        }
+    },
+    computed: {
+        appendHeightCalc: function () {
+            if (this.sclientHeight > this.scrollHeight) {
+                return false
+            }
+            const height = (this.clientHeight + this.scrollTop) - (this.scrollHeight - this.threshold)
+            if (height >= this.threshold / 4 * 3) {
+                return true
+            }
+            return false
         }
     },
     data () {
@@ -57,4 +72,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#infinitescroll {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+span {
+    font-size: 2em;
+}
 </style>
