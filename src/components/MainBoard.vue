30e0db2d5d6fc980b3dfd593bb031beecc34d04d
knh4437 <knh4437@naver.com>
2021-10-01 12:50:24 +0900

    132

--- src/components/MainBoard.vue
+++ src/components/MainBoard.vue
@@ -15,6 +15,7 @@
         </b-col>
       </b-row>
       <b-row align-h="center">
+<<<<<<< Updated upstream
         <b-col cols="12" md="6" lg="5" xl="4" style="margin: 15px;border: 5px dotted black; border-radius: 15px; height: 225px;padding: 5px">
           <!-- TODO: Write post UI component. -->
           Write post UI
@@ -23,6 +24,23 @@
       <b-row>
         <b-col xxl="3" xl="4" lg="6" cols="12" class="b-col" v-for="(post, index) in postList" :key="index">
           <post-box v-bind:post="post" class="post-item"></post-box>
+=======
+        <b-col cols="12" md="6" lg="5" xl="4" style="margin: 15px 0">
+          <div id="writePostUI" style="position: relative">
+            <div>
+              <div class="form-floating">
+                <textarea class="form-control" id="floatingTextarea" style="resize:none; margin-bottom:5px; height:50%"></textarea>
+              </div>
+            </div>
+            <div class="Buttons">
+              <form action="upload" id="uploadForm" method="post" style="display:inline; margin-right:5px">
+                <input type="file" name="file" style="display:none"/>
+                <b-icon icon="camera-fill" type="button" class="camerabtn" onclick="onclick=document.all.file.click()"></b-icon>
+              </form>
+              <b-button type="submit" pill variant="primary" class="postbtn">확인</b-button>
+            </div>
+          </div>
+>>>>>>> Stashed changes
         </b-col>
       </b-row>
     </b-container>
--- src/components/WritePostUI.vue
+++ src/components/WritePostUI.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
     <div>
         <b-container fluid="fluid" style="margin-top:1%; position:relative; left:30% ">
             <b-row>
@@ -25,10 +26,24 @@
                 </b-col>
             </b-row>
         </b-container>
+=======
+    <div id="writePostUI">
+        <div class="form-floating">
+            <textarea v-model="postctx" class="form-control" id="floatingTextarea" style="resize:none; margin-bottom:5px; height:50%"></textarea>
+        </div>
+        <div class="Buttons">
+            <form action="upload" id="uploadForm" method="post" style="display:inline; margin-right:5px">
+                <input type="file" name="file" style="display:none"/>
+                <b-icon icon="camera-fill" type="button" class="camerabtn" onclick="onclick=document.all.file.click()"></b-icon>
+            </form>
+            <b-button type="submit" pill variant="primary" class="postbtn">확인</b-button>
+        </div>
+>>>>>>> Stashed changes
     </div>
 </template>
 <script>
 export default {
+<<<<<<< Updated upstream
     data() {
         return {context: '', images: ''};
     },
@@ -51,5 +66,22 @@ export default {
             container.appendChild(newImage);
         }
     }
+=======
+    data () {
+        return {
+        postctx: ''
+        }
+    }
+}
+</script>
+<style>
+.postBox{
+    resize : none;
+    height : 100px;
+    width : 500px;
+}
+.postbtn{
+    margin : 2px;
+>>>>>>> Stashed changes
 }
 </script>
