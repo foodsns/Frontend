43dad39cb556e2b27b444c4f304c92e6b29956dc
knh4437 <knh4437@naver.com>
2021-10-08 02:11:34 +0900

    Add CropImgvue
    modal 창을 통한 이미지 자르기

--- src/components/WritePostUI.vue
+++ src/components/WritePostUI.vue
@@ -8,20 +8,28 @@
         <label class="btn btn-primary" pill variant="primary" for="filebtn">
         사진
         </label>
-        <input type="file" id="filebtn" @change="uploadPhoto" style="display:none" accept="image/*" multiple/>
+        <input type="file" id="filebtn" @change="uploadPhoto" v-bind:imgList="imgtogo" style="display:none" accept="image/*" multiple/>
+        <CropImg></CropImg>
       </form>
       <button type="submit" class="btn btn-success" id="postbtn">확인</button>
     </div>
   </div>
 </template>
 <script>
+import {EventBus} from '../lib/event-bus.js'
+import {CropImg} from './component/CropImg.vue'
+
 export default {
     name: 'WritePostUI',
+    components: {
+      CropImg
+    },
     data () {
         return {
             inputText: '',
             hashtag: [],
-            hash_count: 0
+            hash_count: 0,
+            imgList: []
         }
     },
     methods: {
@@ -29,13 +37,18 @@ export default {
             console.log('uploadPhoto :', e)
             if (e.target.files) {
                 var fileList = e.target.files
+                var maxamount = 15
                 var maxSize = 3 * 1024 * 1024
+                if (fileList.length > maxamount) {
+                  alert('15개 이하만 업로드 가능합니다.')
+                }
                 for (const file of fileList) {
                     if (file.size > maxSize) {
                         alert(file.name + ' : 3MB이하 이미지만 가능합니다.')
                         return false
                     }
                 }
+                EventBus.$emit('crop-eventbus', this.fileList)
             }
         }
     }
