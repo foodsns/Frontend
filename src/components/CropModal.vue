06d7ed2f5d53a0abbdccc17b720e6f7847c09dc8
knh4437 <knh4437@naver.com>
2021-10-13 19:12:56 +0900

    CropModal

---
+++ src/components/CropModal.vue
@@ -0,0 +1,99 @@
+<template>
+<div class="CropModalUI" v-if="active">
+<!--first modal-->
+    <div class="modal fade" v-if="active" id="firstModalToggle" aria-hidden="true" aria-labelledby="firstModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="firstModalToggle">{{fileList[0].name}}</h5>
+        <button type="button" class="btn-close" @click="$emit('close-modal')" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="croparea">
+          <img v-bind:class="image" src="">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--mid modals-->
+<div class="modal fade" v-if="active" id="midModalToggle" aria-hidden="true" aria-labelledby="midModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="firstModalToggle">{{fileList[0].name}}</h5>
+        <button type="button" class="btn-close" @click="$emit('close-modal')" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Show a second modal and hide this one with the button below.
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!--last modal-->
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
+        <button type="button" class="btn-close" @click="$emit('close-modal')" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Hide this modal and show the first with the button below.
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+import {EventBus} from '../lib/event-bus.js'
+import VueCropper from 'vue-cropperjs'
+
+export default {
+    data () {
+        return {
+            fileList: {},  // fileList
+            active: false,
+            croppedImg: [],
+            beforecrop: null
+        }
+    },
+    created () {
+        EventBus.$on('crop-eventbus', function (fileList) {
+            this.fileList = fileList
+            this.active = true
+        })
+    },
+    method: {
+      cropui: function(index) {
+          if (fileList) {
+            const reader = new Filereader()
+            reader.readAsDataURL(fileList[index])
+            reader.onload = function(event) {
+              image.attr("src", e.target.result)
+              cropper = image.cropper( {
+                dragMode: 'move',
+                vieMode: 1,
+                autoCropArea: 0.7,
+                guides: false
+            })
+          }
+        }
+      }
+    }
+}
+</script>
--- src/components/WritePostUI.vue
+++ src/components/WritePostUI.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="writePostUI" style="position: relative">
     <div class="form-floating">
-      <textarea class="form-control" value="inputText" id="floatingTextarea" placeholder="내용입력" @keyup.#="hashE" enctype="multipart/form-data" style="resize:none; margin-bottom:5px"></textarea>
+      <textarea class="form-control" v-bind:value="inputText" id="floatingTextarea" placeholder="내용입력" v-on:input="updateInput" enctype="multipart/form-data" style="resize:none; margin-bottom:5px"></textarea>
     </div>
     <div class="Buttons">
       <form action="upload" id="uploadForm" method="post" style="display:inline; margin-right:5px">
@@ -9,7 +9,7 @@
         사진
         </label>
         <input type="file" id="filebtn" @change="uploadPhoto" v-bind:imgList="imgtogo" style="display:none" accept="image/*" multiple/>
-        <CropImg></CropImg>
+        <CropModal v-if="isCropModal" @close-modal="isCropModal=false"></CropModal>
       </form>
       <button type="submit" class="btn btn-success" id="postbtn">확인</button>
     </div>
@@ -17,30 +17,29 @@
 </template>
 <script>
 import {EventBus} from '../lib/event-bus.js'
-import {CropImg} from './component/CropImg.vue'
+import {CropModal} from './component/CropModal.vue'
 
 export default {
     name: 'WritePostUI',
     components: {
-      CropImg
+      CropModal
     },
     data () {
         return {
             inputText: '',
-            hashtag: [],
-            hash_count: 0,
-            imgList: []
+            isCropModal: false,
+            hashTag: []
         }
     },
     methods: {
         uploadPhoto: function (e) {
-            console.log('uploadPhoto :', e)
             if (e.target.files) {
                 var fileList = e.target.files
                 var maxamount = 15
                 var maxSize = 3 * 1024 * 1024
                 if (fileList.length > maxamount) {
                   alert('15개 이하만 업로드 가능합니다.')
+                  return false
                 }
                 for (const file of fileList) {
                     if (file.size > maxSize) {
@@ -49,8 +48,18 @@ export default {
                     }
                 }
                 EventBus.$emit('crop-eventbus', this.fileList)
+                this.isCropModal = true;
             }
-        }
+        },
+        /* updateInput: function(e) {
+          var updateInput = e.target.value
+          var splited
+          if (splited = updateInput.split(' ')) { // if user type a spacebar
+            if (splited.index('#')!=-1){ // if there is a hashtag
+            } 
+          }
+          this.inputText = updateInput
+        } */
     }
 }
 
