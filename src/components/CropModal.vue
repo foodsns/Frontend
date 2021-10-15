fb077c8eab96d37df44c085e6fc67c371690800e
Kim HyunWoo <stories2@naver.com>
2021-10-16 03:29:01 +0900

    #51 Fix modal not show up

--- src/components/CropModal.vue
+++ src/components/CropModal.vue
@@ -1,60 +1,17 @@
 <template>
-<div class="CropModalUI">
-<!--first modal-->
-  <div class="modal fade" id="firstModalToggle" aria-labelledby="firstModalToggleLabel" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="firstModalToggle"></h5>
-        <button type="button" class="btn-close" @click="$emit('close-modal')" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="img-container">
-          <img v-bind:src="img">
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
-      </div>
-    </div>
-  </div>
-</div>
 
-<!--mid modals-->
-<div class="modal fade" id="midModalToggle" aria-hidden="true" aria-labelledby="midModalToggleLabel" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="firstModalToggle">{{fileProp.name}}</h5>
-        <button type="button" class="btn-close" @click="$emit('close-modal')" data-bs-dismiss="modal" aria-label="Close"></button>
+  <div class="CropModalUI">
+    <b-modal
+      id="cropper-modal"
+      ref="cropper-modal"
+      title="Submit Your Name"
+    >
+      <div>
+        File name:<span v-if="fileProp">{{fileProp.name}}</span>
+        <img v-if="img" v-bind:src="img" />
       </div>
-      <div class="modal-body">
-        Show a second modal and hide this one with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
-      </div>
-    </div>
+    </b-modal>
   </div>
-</div>
-<!--last modal-->
-<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
-        <button type="button" class="btn-close" @click="$emit('close-modal')" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Hide this modal and show the first with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
 </template>
 
 <script>
@@ -85,7 +42,13 @@ export default {
         }
     },
     mounted () {
-      this.$refs['crop-modal'].show()
+      this.$refs['cropper-modal'].show()
+      // https://stackoverflow.com/a/16153675/7270469
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        this.img = event.target.result
+      }
+      reader.readAsDataURL(this.fileProp)
     },
     method: {
     // cropui: function(index) {
