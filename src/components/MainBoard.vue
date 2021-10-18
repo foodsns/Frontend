e88107fb5ec9882a657a3a9e924726e6342d9341
Kim HyunWoo <stories2@naver.com>
2021-10-18 19:37:38 +0900

    #56 Hot fix, if upload same file then modal will not show up

--- src/components/MainBoard.vue
+++ src/components/MainBoard.vue
@@ -38,7 +38,7 @@
                 <label class="btn btn-primary" pill variant="primary" for="filebtn">
                 사진
                 </label>
-                <input type="file" id="filebtn" @change="uploadOnePhoto" style="display:none" accept="image/*" multiple/>
+                <input type="file" ref="fileInput" id="filebtn" @change="uploadOnePhoto" style="display:none" accept="image/*" multiple/>
               </form>
               <button type="submit" class="btn btn-success" id="postbtn">확인</button>
             </div>
@@ -240,12 +240,16 @@ export default {
       }
     },
     uploadOnePhoto: function (e) {
+      this.cropModal.show = false
       if (e.target.files && e.target.files.length > 0) {
         const file = e.target.files[0]
         const maxSize = 3 * 1024 * 1024
         if (file.size <= maxSize) {
-          this.cropModal.show = true
-          this.cropModal.file = file
+          this.$nextTick(() => {
+            this.cropModal.show = true
+            this.cropModal.file = file
+            e.target.value = ''
+          })
         } else {
           console.warn(`Maximum file size: ${maxSize}, current: ${file.size}`)
         }
