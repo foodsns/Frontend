e8224e63615c03cae0fadb9c2ff22d3118e021d1
Kim HyunWoo <stories2@naver.com>
2021-10-20 02:17:28 +0900

    #58 Call kakao geocoder

--- src/components/userGps.vue
+++ src/components/userGps.vue
@@ -1,27 +1,56 @@
 <template>
     <span class = "userGps">
-        <b-button pill variant="outline-secondary"><font-awesome-icon icon="map-marker-alt"/></b-button>
+        <b-button pill variant="outline-secondary" @click="onLocationLoaderClicked()"><font-awesome-icon icon="map-marker-alt"/></b-button>
+        <div ref="hiddenMap"></div>
     </span>
 </template>
 
 <script>
+import KakaoMapController from '../module/kakaoMap.controller'
 export default {
     name: 'userGps',
+    data () {
+        return {
+            addr1: '',
+            addr2: '',
+            addr3: '',
+            lat: 0,
+            lot: 0,
+            msg: '',
+            kakaoMapInstance: null
+        }
+    },
+    mounted () {
+        this.kakaoMapInstance = new KakaoMapController(this.$refs.hiddenMap)
+        this.kakaoMapInstance.loadScript()
+        .then(() => this.kakaoMapInstance.initMap())
+    },
     methods: {
         getLocation: function () {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function (position) {
-                alert('주소: ' + position.coords.latitude + ' ' + position.coords.longitude)
-                }, function (error) {
-                console.error(error)
-                }, {
-                enableHighAccuracy: false,
-                maximumAge: 0,
-                timeout: Infinity
-                })
-            } else {
-                alert('GPS를 지원하지 않습니다')
-            }
+            return new Promise((resolve, reject) => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition((position) => {
+                        resolve({
+                            lat: position.coords.latitude,
+                            lot: position.coords.longitude
+                        })
+                    }, (error) => {
+                        reject(error)
+                    })
+                } else {
+                    reject(new Error('GPS not supported'))
+                }
+            })
+        },
+        onLocationLoaderClicked: function () {
+            this.getLocation()
+            .then(latLot => this.kakaoMapInstance.latLot2Addr(latLot.lat, latLot.lot))
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.error('err', err)
+            })
         }
     }
 }
