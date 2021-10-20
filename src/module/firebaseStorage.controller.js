a7a91f7a994707c7643cdc78db47ce4c372dda54
Kim HyunWoo <stories2@naver.com>
2021-10-20 20:45:49 +0900

    #58 Upload post image based on doc ID

--- src/module/firebaseStorage.controller.js
+++ src/module/firebaseStorage.controller.js
@@ -8,8 +8,19 @@ export default class FirebaseStorage {
         this.storage = getStorage(firebaseApp)
     }
 
+    generateGuid () {
+        var result, i, j
+        result = ''
+        for (j = 0; j < 32; j++) {
+          if (j === 8 || j === 12 || j === 16 || j === 20) { result = result + '-' }
+          i = Math.floor(Math.random() * 16).toString(16).toUpperCase()
+          result = result + i
+        }
+        return result
+    }
+
     uploadFile (docID, file) {
-        const imgRef = ref(this.storage, `posts/sadfsadf/mountains.jpg`)
+        const imgRef = ref(this.storage, `posts/${docID}/${this.generateGuid()}.${file.name.split('.').pop()}`)
         return uploadBytes(imgRef, file)
         .then((uploadResult) => getDownloadURL(uploadResult.ref))
     }
