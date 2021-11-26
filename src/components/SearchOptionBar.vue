96d8d7bc5f1dead0f76ed5cb0262ed7c114dbc77
Kim HyunWoo <stories2@naver.com>
2021-11-26 18:35:52 +0900

    #142 Remove inline style

--- src/components/SearchOptionBar.vue
+++ src/components/SearchOptionBar.vue
@@ -2,21 +2,21 @@
     <div id="searchOptionBar">
         <b-row>
             <b-col cols="4" md="2" class="option-col" v-if="eanblePublicOrPrivate">
-                <font-awesome-icon icon="globe-asia" style="margin-right:15px;cursor:pointer;" @click="publicOrPrivate = 'public'; emitOptionChanged()"
+                <font-awesome-icon icon="globe-asia" class="icon-btn" @click="publicOrPrivate = 'public'; emitOptionChanged()"
                 v-bind:style="{ opacity: opacityCondition(publicOrPrivate, 'public')}"/>
-                <font-awesome-icon icon="lock" style="margin-right:5px;cursor:pointer;" @click="publicOrPrivate = 'private'; emitOptionChanged()"
+                <font-awesome-icon icon="lock" class="icon-btn" @click="publicOrPrivate = 'private'; emitOptionChanged()"
                 v-bind:style="{ opacity: opacityCondition(publicOrPrivate, 'private')}"/>
             </b-col>
             <b-col cols="4" md="2" class="option-col" v-if="eanbleSortByGoodOrDate">
-                <font-awesome-icon icon="heart" style="margin-right:15px;cursor:pointer;" @click="sortByGoodOrDate = 'good'; emitOptionChanged()"
+                <font-awesome-icon icon="heart" class="icon-btn" @click="sortByGoodOrDate = 'good'; emitOptionChanged()"
                 v-bind:style="{ opacity: opacityCondition(sortByGoodOrDate, 'good')}"/>
-                <font-awesome-icon icon="history" style="margin-right:5px;cursor:pointer;" @click="sortByGoodOrDate = 'date'; emitOptionChanged()"
+                <font-awesome-icon icon="history" class="icon-btn" @click="sortByGoodOrDate = 'date'; emitOptionChanged()"
                 v-bind:style="{ opacity: opacityCondition(sortByGoodOrDate, 'date')}"/>
             </b-col>
             <b-col cols="4" md="2" class="option-col" v-if="eanbleShowAreaOrGlobaly">
-                <font-awesome-icon icon="map-marked" style="margin-right:15px;cursor:pointer;" @click="showAreaOrGlobaly = 'area'; emitOptionChanged()"
+                <font-awesome-icon icon="map-marked" class="icon-btn" @click="showAreaOrGlobaly = 'area'; emitOptionChanged()"
                 v-bind:style="{ opacity: opacityCondition(showAreaOrGlobaly, 'area')}"/>
-                <font-awesome-icon icon="map" style="margin-right:5px;cursor:pointer;" @click="showAreaOrGlobaly = 'global'; emitOptionChanged()"
+                <font-awesome-icon icon="map" class="icon-btn" @click="showAreaOrGlobaly = 'global'; emitOptionChanged()"
                 v-bind:style="{ opacity: opacityCondition(showAreaOrGlobaly, 'global')}"/>
             </b-col>
         </b-row>
@@ -81,5 +81,10 @@ export default {
 .option-col {
     padding: 8px 24px;
     text-align:left;
+}
+
+.icon-btn {
+    margin-right:15px;
+    cursor:pointer;
 }
 </style>
