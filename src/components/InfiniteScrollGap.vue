73a1501f2e8b2ae941415e6ac19f2397a6dd326c
Kim HyunWoo <stories2@naver.com>
2021-12-03 01:25:06 +0900

    #147 Refactoring calculate gap height

--- src/components/InfiniteScrollGap.vue
+++ src/components/InfiniteScrollGap.vue
@@ -33,11 +33,25 @@ export default {
     },
     mounted () {
         // clientHeight + threshold < scrollHeight
-        console.log(`[InfiniteScrollGap] [mounted]`, this.clientHeight, this.scrollHeight, this.thresholdProp)
-        console.log(`testing`, this.$refs.gap)
-        console.log('y', this.$refs.gap.getBoundingClientRect().y)
-        this.gap = Math.abs((this.clientHeight + this.thresholdProp) - this.$refs.gap.getBoundingClientRect().y)
-        console.log('gap', (this.clientHeight + this.thresholdProp), this.gap)
+        // console.log(`[InfiniteScrollGap] [mounted]`, this.clientHeight, this.scrollHeight, this.thresholdProp)
+        // console.log(`testing`, this.$refs.gap)
+        // console.log('y', this.$refs.gap.getBoundingClientRect().y)
+        // this.gap = Math.abs((this.clientHeight + this.thresholdProp) - this.$refs.gap.getBoundingClientRect().y)
+        // console.log('gap', (this.clientHeight + this.thresholdProp), this.gap)
+        window.addEventListener('resize', () => {
+            console.log('resize event')
+            this.gap = this.calcHeightGap()
+        })
+        this.gap = this.calcHeightGap()
+    },
+    methods: {
+        calcHeightGap () {
+            if (!this.$refs.gap || this.clientHeight + this.thresholdProp < this.scrollHeight) {
+                return 0
+            }
+            console.log('ref', this.$refs.gap, !this.$refs.gap)
+            return Math.abs((this.clientHeight + this.thresholdProp) - this.$refs.gap.getBoundingClientRect().y)
+        }
     }
 }
 </script>
